ca30c3b1a2b2802a42fe125ab1c1fc35e627e392
Rui Chen <rui75217@gmail.com>
2018-04-11 12:04:15 +0800

    無法實現自己的想法

--- src/App.js
+++ src/App.js
@@ -10,25 +10,6 @@ class App extends Component {
       { id: 2, name: "Andy", age: 26 }
     ]
   };
-
-  switchNameHandle = name => {
-    this.setState({
-      persons: [
-        { id: 0, name: "Hello", age: 119 },
-        { id: 1, name: name, age: 116 },
-        { id: 2, name: "React", age: 126 }
-      ]
-    });
-  };
-  handleChangeName = e => {
-    this.setState({
-      persons: [
-        { id: 0, name: e.target.value, age: 119 },
-        { id: 1, name: e.target.value, age: 116 },
-        { id: 2, name: e.target.value, age: 126 }
-      ]
-    });
-  };
   render() {
     const btnStyle = {
       backgroundColor: "white",
@@ -40,25 +21,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button style={btnStyle} onClick={() => this.switchNameHandle("Node")}>
-          Switch Name
-        </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          changeName={this.handleChangeName}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandle.bind(this, "JavaScript")}
-          changeName={this.handleChangeName}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-          changeName={this.handleChangeName}
-        />
+        {this.state.persons.map((person, index) => (
+          <Person key={index} id={index} name={person.name} age={person.age} />
+        ))}
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "./Person.css";
-const person = ({ name, age, click, changeName }) => {
-  // this.handleId = e => {
-  //   console.log(e);
-  // };
+const person = ({ name, age, id }) => {
   return (
-    <div className="Person">
-      <p onClick={click}>
-        My name is {name}, and my age is {age}
+    <div className="Person" id={id}>
+      <p>
+        My name is {name}, and age is {age}
       </p>
-      <input type="text" onChange={changeName} value={name} />
+      <input type="text" />
     </div>
   );
 };
